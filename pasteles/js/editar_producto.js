f5cf718a1b1dc20cd631b7a7a9395a6776934152
ines <inesmanriqv@gmail.com>
2023-12-03 02:48:25 -0300

    Proyecto backend

---
+++ pasteles/js/editar_producto.js
@@ -0,0 +1,59 @@
+console.log(location.search) // lee los argumentos pasados a este formulario
+var id=location.search.substr(4)
+console.log(id)
+const { createApp } = Vue
+createApp({
+data() {
+return {
+id:0,
+tipo:"",
+imagen:"",
+stock:0,
+precio:0,
+url:'https://maria20232023.pythonanywhere.com/productos/'+id,
+}
+},
+methods: {
+fetchData(url) {
+fetch(url)
+.then(response => response.json())
+.then(data => {console.log(data)
+    this.id=data.id
+    this.tipo=data.tipo
+    this.imagen=data.imagen
+    this.stock=data.stock
+    this.precio=data.precio
+    })
+    .catch(err => {
+    console.error(err);
+    this.error=true
+    })
+    },
+    modificar() {
+    let producto = {
+    tipo:this.tipo,
+    precio: this.precio,
+    stock: this.stock,
+    imagen:this.imagen
+    }
+    var options = {
+    body: JSON.stringify(producto),
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    redirect: 'follow'
+    }
+    fetch(this.url, options)
+    .then(function () {
+    alert("Registro modificado")
+    window.location.href = "./index.html";
+    })
+    .catch(err => {
+    console.error(err);
+    alert("Error al Modificar")
+    })
+    }
+    },
+    created() {
+    this.fetchData(this.url)
+    },
+    }).mount('#app')
---
+++ pasteles/js/main.js
@@ -0,0 +1,35 @@
+document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #FFACB7;">
+<div class="container">
+  <a class="navbar-brand" href="index.html"> <img width="120" height="80" src="/img/logoCaC2.png"></a>
+  <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
+  data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+  aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavId">
+  <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+
+  
+  <li class="nav-item">
+  <a class="nav-link active" href="index.html" aria-current="page">Home <span
+  
+  class="visually-hidden">(current)</span></a>
+  </li>
+ 
+
+
+  <li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle" href="#" id="dropdownId"
+  data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">CRUD</a>
+  <div class="dropdown-menu" aria-labelledby="dropdownId">
+  <a class="dropdown-item" href="index.html">Productos</a>
+  <a class="dropdown-item" href="producto_nuevo.html">Nuevo producto</a>
+  <a class="dropdown-item" href="producto_update.html">Editar producto</a>
+  </div>
+  </li>
+  </ul>
+ <h2>Sabor a Mi</h2>
+  </div>
+  </div>
+  </nav>
+  `
