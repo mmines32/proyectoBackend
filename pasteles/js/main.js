4c6218960485566e6085d388047abad36f806530
ines <inesmanriqv@gmail.com>
2023-12-03 16:23:08 -0300

    Proyecto finalizado

--- pasteles/js/main.js
+++ pasteles/js/main.js
@@ -1,13 +1,15 @@
 document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #FFACB7;">
 <div class="container">
-  <a class="navbar-brand" href="index.html"> <img width="120" height="80" src="/img/logoCaC2.png"></a>
+  <a class="navbar-brand" href="https://mmines32.github.io/ProyectoHTMLCodoACodo1"> <img width="120" height="80" src="/img/logoCaC2.png"></a>
   <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
   data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
   aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
+  
   </button>
   <div class="collapse navbar-collapse" id="collapsibleNavId">
   <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+
 
   
   <li class="nav-item">
@@ -19,7 +21,7 @@ document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-l
 
 
   <li class="nav-item dropdown">
-  <a class="nav-link dropdown-toggle" href="#" id="dropdownId"
+  <a class="nav-link dropdown-toggle" href="#"id="dropdownId"
   data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">CRUD</a>
   <div class="dropdown-menu" aria-labelledby="dropdownId">
   <a class="dropdown-item" href="index.html">Productos</a>
